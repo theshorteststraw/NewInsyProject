6725d86d756c4eccb490749c18fc27a5908f3aa3
Denny Stiles <dennis.stiles@marquette.edu>
2024-04-25 15:07:56 -0500

    Add Customers Class

---
+++ Appointment.cs
@@ -0,0 +1,9 @@
+﻿namespace NewInsyProject;
+
+public class Appointment
+{
+public int Id { get; set; }
+    public DateTime dateTime {get; set;}
+    public string Description { get; set; }
+
+}
---
+++ Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace NewInsyProject;
+
+public class Customer
+{
+ public Customer c { get; set; }
+    public Appointment a { get; set; }
+    
+    public Customer(Customer c, Appointment a)
+    {
+        this.c = c;
+        this.a = a;
+    }
+}
