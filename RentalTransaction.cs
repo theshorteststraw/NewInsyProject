f8c293afb5f45b4c30b5349bffb7e20978ec6e9b
Denny Stiles <dennis.stiles@marquette.edu>
2024-04-30 14:58:20 -0500

    RentalTransaction

---
+++ RentalTransaction.cs
@@ -0,0 +1,16 @@
+﻿namespace NewInsyProject;
+
+public class RentalTransaction
+{
+    public int Id { get; set; }
+
+    public Customer Customer { get; set; }
+
+    public Car Car { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public DateTime ReturnDate { get; private set; }
+}
