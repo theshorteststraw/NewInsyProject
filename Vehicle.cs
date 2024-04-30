28cdd46c000e72697bc95a437bf1dcc7c2cb36e0
theshorteststraw <aloe.school.acct@gmail.com>
2024-04-30 14:33:09 -0500

    create Vehicle class

---
+++ Vehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace NewInsyProject;
+
+public class CustomerAppointment
+{
+    public Customer c {get; set;}
+
+    public Appointment a {get; set;}
+
+    public CustomerAppointment(Customer c, CustomerAppointment a)
+    {
+        this.c = c;
+        this.a = a;
+    }
+}
