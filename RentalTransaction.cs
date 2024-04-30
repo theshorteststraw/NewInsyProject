6ae52c9d25884e107e32cb3c1f2228bd97cabcb3
dennystiles <167247161+dennystiles@users.noreply.github.com>
2024-04-30 15:18:06 -0500

    Update RentalTransaction.cs

--- RentalTransaction.cs
+++ RentalTransaction.cs
@@ -6,7 +6,7 @@ public class RentalTransaction
 
     public Customer Customer { get; set; }
 
-    public Vehicle vehicle { get; set; }
+    public Vehicle Vehicle { get; set; }
 
     public DateTime StartDate { get; set; }
 
